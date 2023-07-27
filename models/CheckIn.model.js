0e1d6db6c0226e883a4f8b0cb86d4bddebe8e209
Andy Cockroft <acockroft1994@gmail.com>
2023-07-27 15:03:50 +0200

    completed user checkin

--- models/CheckIn.model.js
+++ models/CheckIn.model.js
@@ -5,8 +5,8 @@ const CheckInSchema = new Schema({
     mood: { type: Number },
     imageUrl: { type: String},
     audioUrl: { type: String },
-    diaryText: [{ type: String }],
-    user: [{ type: Schema.Types.ObjectId, ref: "User"}]
+    diaryEntry: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "User"},
 });
 
 module.exports = model("CheckIn", CheckInSchema);
--- routes/userAuth.routes.js
+++ routes/userAuth.routes.js
@@ -49,15 +49,15 @@ router.post('/signup', (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then` 
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword, name, checkInId: [] });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, name, _id, checkInId } = createdUser;
     
       // Create a new object that doesn't expose the password
-      const payload = { email, name, _id };
+      const payload = { email, name, _id, checkInId };
 
       // Create and sign the token
       const authToken = jwt.sign( 

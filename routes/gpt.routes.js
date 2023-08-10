e146fdb0ee49f7d0c704b76a377c02a69b6451af
Andy Cockroft <acockroft1994@gmail.com>
2023-08-10 19:34:46 +0200

    fourth attempt, clearing server error and declaring fetch differently

--- routes/gpt.routes.js
+++ routes/gpt.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -10,26 +9,31 @@ const chatAPIKey = process.env.OPENAI_API_KEY;
 router.use(express.json());
 
 router.post("/completions", async (req, res, next) => {
-    const options = {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${chatAPIKey}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are an empathetic companion on an emotional wellbeing app. You offer mental health tips and advice to users based on their prompts. Do not assume the role of a professional and remind them of that.",
-        },
-        { role: "user", content: req.body.message }],
-      max_tokens: 500,
-    }),
-  };
   try {
-    const response = await fetch(
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${chatAPIKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an empathetic companion on an emotional wellbeing app. You offer mental health tips and advice to users based on their prompts. Do not assume the role of a professional and remind them of that.",
+          },
+          { role: "user", content: req.body.message },
+        ],
+        max_tokens: 500,
+      }),
+    };
+
+    // Use dynamic import() to load node-fetch as an ES module
+    const fetch = await import("node-fetch");
+
+    const response = await fetch.default(
       "https://api.openai.com/v1/chat/completions",
       options
     );
@@ -37,8 +41,8 @@ router.post("/completions", async (req, res, next) => {
     res.send(data);
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "An error occurred while processing the request." });
   }
 });
-
 
 module.exports = router;
